3a09e9bc64f1befedee691d379ed6cf16fc764f2
Marten de Bruijn <martenbruijn@gmail.com>
2020-03-23 15:51:19 +0100

    added pre-caching

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,28 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/css/styles.css',
+  'https://fonts.googleapis.com/css?family=Roboto&display=swap',
+  'https://kit.fontawesome.com/45727ea779.js',
+];
+
 // install service worker
 self.addEventListener('install', evt => {
-  console.log('service worker has been installed');
+  //   console.log('service worker has been installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
 // activate event
 self.addEventListener('activate', evt => {
-  console.log('service worker has been activated');
+  //   console.log('service worker has been activated');
+});
+
+// fetch events
+self.addEventListener('fetch', evt => {
+  //   console.log('fetch event', evt);
 });
